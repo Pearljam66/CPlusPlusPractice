769566906d956b2e399b7c62808973bfeb489615
Sarah Clark <sarahireneclark@gmail.com>
2024-04-16 15:59:45 -0500

    Initial commit of project

---
+++ SwiftAndCPlusPlus/SwiftAndCPlusPlus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SwiftAndCPlusPlus
+//
+//  Created by Sarah Clark on 4/16/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
