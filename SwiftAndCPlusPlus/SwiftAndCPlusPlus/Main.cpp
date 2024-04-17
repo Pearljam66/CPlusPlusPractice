7cb8e99d2288ed7a9a83728263c10405f73ed8eb
Sarah Clark <sarahireneclark@gmail.com>
2024-04-16 22:37:41 -0500

    Listing 2.4

--- SwiftAndCPlusPlus/SwiftAndCPlusPlus/Main.cpp
+++ SwiftAndCPlusPlus/SwiftAndCPlusPlus/Main.cpp
@@ -5,16 +5,24 @@
 //  Created by Sarah Clark on 4/16/24.
 //
 
-// Lesson 2.3
-// Another demonstration of the using keyword
+// Lesson 2.4
+// Declaring, defining, and calling a function that demonstrates capabilities of std::cout.
 #include <iostream>
+using namespace std;
+
+int demoConsoleOutput();
 
 int main() {
-    using std::cout;
-    using std::endl;
-
-    cout << "Hello World" << endl;
+    demoConsoleOutput();
 
     return 0;
 }
 
+int demoConsoleOutput() {
+    cout << "This is a simple string literal" << endl;
+    cout << "Writing number five: " << 5 << endl;
+    cout << "Performing division 10 / 5 = " << 10 / 5 << endl;
+    cout << "Pi is 22 / 7 = " << 22.0 / 7 << endl;
+
+    return 0;
+}
