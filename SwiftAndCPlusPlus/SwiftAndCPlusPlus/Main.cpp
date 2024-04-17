eca148e63e6e73e7f90a219c23bdfe4734f65086
Sarah Clark <sarahireneclark@gmail.com>
2024-04-16 21:45:03 -0500

    Lesson 2.1

--- SwiftAndCPlusPlus/SwiftAndCPlusPlus/Main.cpp
+++ SwiftAndCPlusPlus/SwiftAndCPlusPlus/Main.cpp
@@ -5,20 +5,17 @@
 //  Created by Sarah Clark on 4/16/24.
 //
 
+// Lesson 2.1
+
+// Preprocessor directive that includes header iostream
 #include<iostream>
 
-/*int main() {
-    int x = 8;
-    int y = 6;
-    std::cout << std::endl;
-    std::cout << x - y << " " << x * y << " " << x + y;
-    std::cout << std::endl;
-    return 0;
-}*/
-
-// clang++ -o hello Hello.cpp
+// Start of your program: function block main()
 
 int main() {
-    std::cout << "Hello Buggy World " << std::endl;
+    /* Write to the console output i.e. screen */
+    std::cout << "Hello World" << std::endl;
+
+    // Return a value to the OS.
     return 0;
 }
