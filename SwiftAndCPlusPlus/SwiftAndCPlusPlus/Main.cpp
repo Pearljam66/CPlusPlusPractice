e0e6a4d8c6c61fdb84c38c229fb0a6e674bf9353
Sarah Clark <sarahireneclark@gmail.com>
2024-04-16 20:46:31 -0500

    Lesson one

---
+++ SwiftAndCPlusPlus/SwiftAndCPlusPlus/Main.cpp
@@ -0,0 +1,24 @@
+//
+//  Hello.cpp
+//  SwiftAndCPlusPlus
+//
+//  Created by Sarah Clark on 4/16/24.
+//
+
+#include<iostream>
+
+/*int main() {
+    int x = 8;
+    int y = 6;
+    std::cout << std::endl;
+    std::cout << x - y << " " << x * y << " " << x + y;
+    std::cout << std::endl;
+    return 0;
+}*/
+
+// clang++ -o hello Hello.cpp
+
+int main() {
+    std::cout << "Hello Buggy World " << std::endl;
+    return 0;
+}
